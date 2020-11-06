48d93645b61f1767060fd458c187e4b3ed503dd7
ravisharma375 <somethingfferent375@gmail.com>
2020-11-05 23:49:55 -0800

    hello

--- config/database.js
+++ config/database.js
@@ -1,61 +1,61 @@
-const { Sequelize } = require("sequelize");
-const { config } = require("./index");
-const productModel = require("../model/product");
-const userModel = require("../model/user");
-const sequelize = new Sequelize(
-  config.get("DATABASE_NAME"),
-  config.get("DATABASE_USERNAME"),
-  config.get("DATABASE_PASSWORD"),
-  {
-    host: config.get("DATABASE_HOST"),
-    port: config.get("DATABASE_PORT"),
-    dialect: config.get("DATABASE_DIALECT"),
-    logging: console.log,
-    sync: false,
-    //India TimeZone Change
-    // timezone: "Asia/Calcutta",
-    // dialectOptions: {
-    //   useUTC: false,
-    //   // timezone: "local",
-    // },
-    pool: {
-      max: config.get("DATABASE_POOL_MAX"),
-      min: config.get("DATABASE_POOL_MIN"),
-      idle: config.get("DATABASE_POOL_IDLE"),
-    },
-  },
-);
-const Products = productModel(sequelize, Sequelize);
-const User = userModel(sequelize, Sequelize);
-if (process.env.NODE_ENV === "development") {
-  sequelize
-    .sync({ force: false })
-    .then(() => {
-      console.log("Sequelize running in sync mode");
-    })
-    .catch(error => {
-      throw error;
-    });
-} else {
-  // Temporary. Use migrations in production
-  sequelize
-    .sync({ force: false })
-    .then(() => {
-      console.log("Sequelize running in alter mode");
-    })
-    .catch(error => {
-      throw error;
-    });
-}
+// const { Sequelize } = require("sequelize");
 
-const shutdownDatabase = () => {
-  sequelize.close();
-};
+// const productModel = require("../model/product");
+// const userModel = require("../model/user");
+// const sequelize = new Sequelize(
+//   config.get("DATABASE_NAME"),
+//   config.get("DATABASE_USERNAME"),
+//   config.get("DATABASE_PASSWORD"),
+//   {
+//     host: config.get("DATABASE_HOST"),
+//     port: config.get("DATABASE_PORT"),
+//     dialect: config.get("DATABASE_DIALECT"),
+//     logging: console.log,
+//     sync: false,
+//     //India TimeZone Change
+//     // timezone: "Asia/Calcutta",
+//     // dialectOptions: {
+//     //   useUTC: false,
+//     //   // timezone: "local",
+//     // },
+//     pool: {
+//       max: config.get("DATABASE_POOL_MAX"),
+//       min: config.get("DATABASE_POOL_MIN"),
+//       idle: config.get("DATABASE_POOL_IDLE"),
+//     },
+//   },
+// );
+// const Products = productModel(sequelize, Sequelize);
+// const User = userModel(sequelize, Sequelize);
+// if (process.env.NODE_ENV === "development") {
+//   sequelize
+//     .sync({ force: false })
+//     .then(() => {
+//       console.log("Sequelize running in sync mode");
+//     })
+//     .catch(error => {
+//       throw error;
+//     });
+// } else {
+//   // Temporary. Use migrations in production
+//   sequelize
+//     .sync({ force: false })
+//     .then(() => {
+//       console.log("Sequelize running in alter mode");
+//     })
+//     .catch(error => {
+//       throw error;
+//     });
+// }
 
-module.exports = {
-  shutdownDatabase,
-  config,
-  sequelize,
-  Products,
-  User,
-};
+// const shutdownDatabase = () => {
+//   sequelize.close();
+// };
+
+// module.exports = {
+//   shutdownDatabase,
+//   config,
+//   sequelize,
+//   Products,
+//   User,
+// };
--- routes/index.js
+++ routes/index.js
@@ -1,110 +1,150 @@
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
-//const { Products, config, User } = require("../config/database");
-var passport = require("passport");
-// var auth = require("../config/auth");
-// const { Op } = require("sequelize");
-// UUID to generate unique key
-const { v4: uuid } = require("uuid");
 
 // WARNING: Add a Stripe Key
-//const stripe = require("stripe")(config.get("SECRET_KEY"));
+const stripe = require("stripe")(process.env.SECRET_KEY);
 
 //post register
-router.post("/register", async function(req, res) {
-  const { email, password } = req.body;
+// router.post("/register", async function(req, res) {
+//   const { email, password } = req.body;
 
-  console.log(email, password);
-  if (password == "" || email == "") {
-    return res.json({
-      error: true,
-      message: "missing credential",
-      status: false,
-    });
-  }
-  if (password.length < 6) {
-    return res.json({
-      error: true,
-      message: "Password grater then  6 charater",
-      status: false,
-    });
-  }
-  try {
-    const findUser = await User.findOne({
-      where: {
-        email: email,
-      },
-    });
-    if (findUser) {
-      return res.json({
-        error: true,
-        message: "User Allready Exist !",
-        status: false,
-      });
-    }
-    const result = await User.create({
-      email: email,
-      password: User.hashPassword(password),
-    });
+//   console.log(email, password);
+//   if (password == "" || email == "") {
+//     return res.json({
+//       error: true,
+//       message: "missing credential",
+//       status: false,
+//     });
+//   }
+//   if (password.length < 6) {
+//     return res.json({
+//       error: true,
+//       message: "Password grater then  6 charater",
+//       status: false,
+//     });
+//   }
+//   try {
+//     const findUser = await User.findOne({
+//       where: {
+//         email: email,
+//       },
+//     });
+//     if (findUser) {
+//       return res.json({
+//         error: true,
+//         message: "User Allready Exist !",
+//         status: false,
+//       });
+//     }
+//     const result = await User.create({
+//       email: email,
+//       password: User.hashPassword(password),
+//     });
 
-    if (result) {
-      return res.json({
-        error: false,
-        message: "SuccessFully Register",
-        status: true,
-      });
-    }
-    return false;
-  } catch (error) {
-    console.log(error);
-    return res.json({
-      error: true,
-      message: "Internal Server Error",
-      status: false,
-    });
-  }
-});
+//     if (result) {
+//       return res.json({
+//         error: false,
+//         message: "SuccessFully Register",
+//         status: true,
+//       });
+//     }
+//     return false;
+//   } catch (error) {
+//     console.log(error);
+//     return res.json({
+//       error: true,
+//       message: "Internal Server Error",
+//       status: false,
+//     });
+//   }
+// });
 //post login
 //passport auth
-router.post(
-  "/Login",
-  passport.authenticate("local", {
-    successRedirect: "/home",
-    failureRedirect: "/Login",
-    failureFlash: true,
-  }),
-  (req, res) => {
-    const { user } = req;
+// router.post(
+//   "/Login",
+//   passport.authenticate("local", {
+//     successRedirect: "/home",
+//     failureRedirect: "/Login",
+//     failureFlash: true,
+//   }),
+//   (req, res) => {
+//     const { user } = req;
 
-    if (user) {
-      console.log("You are logged in!");
-      sendResponse(0, user.id, res);
-      // return res.redirect('http://www.google.com')
-    }
-  },
-);
-const routes = [
-  {
-    path: "/home",
-  },
-];
-function sendResponse(roleIndex, userId, res) {
-  return res.redirect(routes[roleIndex].path);
-}
+//     if (user) {
+//       console.log("You are logged in!");
+//       sendResponse(0, user.id, res);
+//       // return res.redirect('http://www.google.com')
+//     }
+//   },
+// );
+// const routes = [
+//   {
+//     path: "/home",
+//   },
+// ];
+// function sendResponse(roleIndex, userId, res) {
+//   return res.redirect(routes[roleIndex].path);
+// }
 //logout
-router.get("/Logout", function(req, res) {
-  req.logout();
-  const msg = req.flash();
-  console.log(msg.error);
-  res.render("index", {
-    loggedOut: "no",
-    title: "Login",
-    message: msg.error,
-  });
-});
+// router.get("/Logout", function(req, res) {
+//   req.logout();
+//   const msg = req.flash();
+//   console.log(msg.error);
+//   res.render("index", {
+//     loggedOut: "no",
+//     title: "Login",
+//     message: msg.error,
+//   });
+// });
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Express", publicKey: process.env.PUBLISHABLE_KEY });
+});
+var YOUR_DOMAIN = process.env.YOUR_DOMAIN;
+router.get("/success", async (req, res) => {
+  const { sessionId } = req.query;
+  const session = await stripe.checkout.sessions.retrieve(sessionId);
+  const orderData = {
+    order_id: session.id,
+    customer_id: session.customer,
+    payment_type: session.payment_method_types[0],
+    payment_status: session.payment_status,
+    amount: session.amount_total / 100 + "₹",
+  };
+  return res.json(orderData);
+  res.render("success");
+});
+router.post("/create-session", async (req, res) => {
+  const { body } = req;
+  console.log(body);
+  // const data = {
+  //   productName: body.product_name,
+  //   amount: body.amount,
+  //   quantity: body.quantity_number,
+  // };
+  const session = await stripe.checkout.sessions.create({
+    submit_type: "pay",
+    billing_address_collection: "auto",
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "INR",
+          product_data: {
+            name: "Stubborn Attachments",
+            images: ["https://i.imgur.com/EHyR2nP.png"],
+          },
+          unit_amount: body.amount * 100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${YOUR_DOMAIN}/success?sessionId={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${YOUR_DOMAIN}/cancel`,
+  });
+  console.log(session);
+  res.json({ id: session.id });
 });
 module.exports = router;
